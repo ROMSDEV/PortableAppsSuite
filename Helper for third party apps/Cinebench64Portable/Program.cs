429997afc6e607439cc8f2f9a0873bdfd490c795
Si13n7 <admin@si13n7.com>
2015-09-24 21:23:43 +0200

    CINEBENCH profile added.

---
+++ Helper for third party apps/Cinebench64Portable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace Cinebench64Portable // CINEBENCH_R15
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            bool newInstance = true;
+            using (Mutex mutex = new Mutex(true, Process.GetCurrentProcess().ProcessName, out newInstance))
+            {
+                if (newInstance)
+                {
+                    SilDev.Log.AllowDebug();
+                    SilDev.Run.App(new ProcessStartInfo() { FileName = "%CurrentDir%\\App\\cinebench64\\CINEBENCH Windows 64 Bit.exe" }, 0);
+                    while (Process.GetProcessesByName("CINEBENCH Windows 64 Bit").Length > 0)
+                        foreach (Process app in Process.GetProcessesByName("CINEBENCH Windows 64 Bit"))
+                            app.WaitForExit();
+                    try
+                    {
+                        string cachePath = SilDev.Run.EnvironmentVariableFilter("%AppData%\\MAXON");
+                        foreach (var dir in Directory.GetDirectories(cachePath, "cinebench64_*", SearchOption.TopDirectoryOnly))
+                            Directory.Delete(dir, true);
+                        if (Directory.GetFiles(cachePath, "*", SearchOption.AllDirectories).Length == 0)
+                            Directory.Delete(cachePath, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        SilDev.Log.Debug(ex);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Helper for third party apps/Cinebench64Portable/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CINEBENCH (64-bit) Portable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyProduct("Cinebench64Portable")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2015")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("021d99cb-9f04-4f9c-8c73-2dacbfc691af")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
