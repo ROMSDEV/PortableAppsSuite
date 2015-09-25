e88bb09e2b2af663a893bbe0030931e20dcb12cd
Si13n7 <admin@si13n7.com>
2015-09-25 03:23:21 +0200

    VLC profile added - PortableApps code is outdated crap.

---
+++ Helper for third party apps/VLCPortable/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace VLCPortable
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+#if x86
+            string appPath = SilDev.Run.EnvironmentVariableFilter("%CurrentDir%\\App\\vlc\\vlc.exe");
+#else
+            string appPath = SilDev.Run.EnvironmentVariableFilter("%CurrentDir%\\App\\vlc64\\vlc.exe");
+#endif
+            bool newInstance = true;
+            using (Mutex mutex = new Mutex(true, Process.GetCurrentProcess().ProcessName, out newInstance))
+            {
+                SilDev.Log.AllowDebug();
+                string cmdLine = null;
+                int length = Environment.GetCommandLineArgs().Length - 1;
+                if (length > 0)
+                {
+                    string[] cmdLineArgs = new string[length];
+                    Array.ConstrainedCopy(Environment.GetCommandLineArgs(), 1, cmdLineArgs, 0, length);
+                    cmdLine = string.Format("\"{0}\" ", string.Join("\" \"", cmdLineArgs));
+                }
+                if (newInstance)
+                {
+                    SilDev.Data.DirLink(SilDev.Run.EnvironmentVariableFilter("%AppData%\\vlc"), SilDev.Run.EnvironmentVariableFilter("%CurrentDir%\\Data"), true);
+                    SilDev.Run.App(new ProcessStartInfo() { FileName = appPath, Arguments = string.Format("{0}--no-plugins-cache", cmdLine) }, 0);
+                    while (Process.GetProcessesByName("vlc").Length > 0)
+                    {
+                        foreach (Process app in Process.GetProcessesByName("vlc"))
+                            app.WaitForExit();
+                    }
+                    SilDev.Data.DirUnLink(SilDev.Run.EnvironmentVariableFilter("%AppData%\\vlc"), true);
+                }
+                else
+                    SilDev.Run.App(new ProcessStartInfo() { FileName = appPath, Arguments = cmdLine }, 0);
+            }
+        }
+    }
+}
---
+++ Helper for third party apps/VLCPortable/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if x86
+[assembly: AssemblyTitle("VLC Media Player Portable")]
+[assembly: AssemblyProduct("VLCPortable")]
+#else
+[assembly: AssemblyTitle("VLC Media Player (64-bit) Portable")]
+[assembly: AssemblyProduct("VLC64Portable")]
+#endif
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2015")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd2d62d9-f7fb-4699-bc3f-5af000419673")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
