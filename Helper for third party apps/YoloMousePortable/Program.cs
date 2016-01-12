c4df2e6f66deac81eeced56b496e8aac6aa64571
Si13n7 <si13n7@live.de>
2016-01-12 01:11:15 +0100

    Yolo Mouse profile added.

---
+++ Helper for third party apps/YoloMousePortable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace YoloMousePortable
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            string appPath = Path.Combine(Application.StartupPath, "App\\YoloMouse\\YoloMouse.exe");
+            if (!File.Exists(appPath) || Process.GetProcessesByName(Path.GetFileNameWithoutExtension(appPath)).Length > 0)
+                Environment.Exit(-1);
+            bool newInstance = true;
+            using (Mutex mutex = new Mutex(true, Process.GetCurrentProcess().ProcessName, out newInstance))
+            {
+                if (newInstance)
+                {
+                    SilDev.Log.AllowDebug();
+                    string dataDir = SilDev.Run.EnvironmentVariableFilter("%CurrentDir%\\Data");
+                    string defDataDir = SilDev.Run.EnvironmentVariableFilter("%LocalAppData%\\YoloMouse");
+                    SilDev.Data.DirLink(defDataDir, dataDir, true);
+                    SilDev.Run.App(new ProcessStartInfo() { FileName = appPath }, 0);
+                    SilDev.Data.DirUnLink(defDataDir, true);
+                }
+            }
+        }
+    }
+}
---
+++ Helper for third party apps/YoloMousePortable/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Yolo Mouse Portable")]
+[assembly: AssemblyProduct("YoloMousePortable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2016")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0689c34e-2d46-4c05-bd37-58b5d4a55479")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
