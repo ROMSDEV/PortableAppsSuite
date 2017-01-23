c47fa921c5fe1affba9036bbf6f90173040eea13
Si13n7 <si13n7@live.de>
2017-01-23 06:26:30 +0100

    OBSStudio: Add support to merge x86 and x64 bit versions.

--- Helper for third party apps/OBSStudioPortable/Program.cs
+++ Helper for third party apps/OBSStudioPortable/Program.cs
@@ -12,26 +12,36 @@ namespace OBSPortable
         private static void Main()
         {
             Log.AllowLogging();
+
+#if x86
+            var curPath64 = PathEx.Combine(PathEx.LocalDir, "OBSStudio64Portable.exe");
+            if (Environment.Is64BitOperatingSystem && File.Exists(curPath64))
+            {
+                ProcessEx.Start(curPath64, EnvironmentEx.CommandLine());
+                return;
+            }
+            var appPath = PathEx.Combine(PathEx.LocalDir, @"App\OBS32\bin\32bit\obs32.exe");
+#else
+            var appPath = PathEx.Combine(PathEx.LocalDir, @"App\OBS64\bin\64bit\obs64.exe");
+#endif
+
+            if (!File.Exists(appPath) || Process.GetProcessesByName(Path.GetFileNameWithoutExtension(appPath)).Length > 0)
+                return;
+
             bool newInstance;
             using (new Mutex(true, Process.GetCurrentProcess().ProcessName, out newInstance))
+            {
                 if (newInstance)
-                {
-                    var appPath = PathEx.Combine(PathEx.LocalDir,
-#if x86
-                     @"App\OBS32\bin\32bit\obs32.exe"
-#else
-                     @"App\OBS64\bin\64bit\obs64.exe"
-#endif
-                    );
+                    return;
 
-                    if (!File.Exists(appPath) || Process.GetProcessesByName(Path.GetFileNameWithoutExtension(appPath)).Length > 0)
-                        return;
-                    Data.DirLink(@"%AppData%\obs-studio", @"%CurDir%\Data", true);
-                    using (var p = ProcessEx.Start(appPath, false, false))
-                        if (!p?.HasExited == true)
-                            p?.WaitForExit();
-                    Data.DirUnLink(@"%AppData%\obs-studio", true);
-                }
+                Data.DirLink(@"%AppData%\obs-studio", @"%CurDir%\Data", true);
+
+                using (var p = ProcessEx.Start(appPath, false, false))
+                    if (!p?.HasExited == true)
+                        p?.WaitForExit();
+
+                Data.DirUnLink(@"%AppData%\obs-studio", true);
+            }
         }
     }
 }
