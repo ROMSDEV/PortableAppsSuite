6dfcb38860246f702b6c1e5114fa1f9cdaf820e9
Si13n7 <si13n7@live.de>
2017-03-20 02:10:59 +0100

    Discord: Add project files.

---
+++ Helper for third party apps/DiscordPortable/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace DiscordPortable
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using SilDev;
+
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Log.AllowLogging();
+            bool newInstance;
+            using (new Mutex(true, Process.GetCurrentProcess().ProcessName, out newInstance))
+            {
+                if (!newInstance)
+                    return;
+
+                var defaultKeys = new[]
+                {
+                    "HKCR\\Discord"
+                };
+
+                var backupKeys = new[]
+                {
+                    "HKCR\\SI13N7-BACKUP: Discord"
+                };
+
+                if (Elevation.IsAdministrator && Environment.CommandLine.ContainsEx("{E429830E-A5A1-4FA0-9D5F-B21964F4698E}"))
+                    goto ClearRegistry;
+
+                for (var i = 0; i < defaultKeys.Length; i++)
+                    if (!Reg.ValueExist(defaultKeys[i], "Portable App"))
+                        Reg.MoveSubKey(defaultKeys[i], backupKeys[i].Substring(5));
+
+                if (!Elevation.IsAdministrator && defaultKeys.Any(Reg.SubKeyExist))
+                {
+                    Elevation.RestartAsAdministrator();
+                    return;
+                }
+
+                var defAppDir = PathEx.Combine("%LocalAppData%\\Discord");
+                var appDir = PathEx.Combine(PathEx.LocalDir, "App\\Discord");
+                Data.DirLink(defAppDir, appDir, true);
+
+                var defTmpDir = PathEx.Combine("%LocalAppData%\\SquirrelTemp");
+                var tmpDir = Path.Combine(appDir, "packages\\SquirrelTemp");
+                Data.DirLink(defTmpDir, tmpDir, true);
+
+                var defDataDir = PathEx.Combine("%AppData%\\discord");
+                var dataDir = PathEx.Combine(PathEx.LocalDir, "Data\\AppData");
+                Data.DirLink(defDataDir, dataDir, true);
+
+                var regPath = PathEx.Combine(PathEx.LocalDir, "Data\\settings.reg");
+                Reg.ImportFile(regPath);
+
+                var appPath = Path.Combine(appDir, "Update.exe");
+                using (var p = ProcessEx.Start(appPath, "--processStart Discord.exe", false, false))
+                    if (!p?.HasExited == true)
+                        p?.WaitForExit();
+
+                var fList = new List<string>();
+                foreach (var d in Directory.GetDirectories(appDir, "app-*", SearchOption.TopDirectoryOnly))
+                    fList.AddRange(Directory.GetFiles(d, "*.exe", SearchOption.TopDirectoryOnly));
+                for (var i = 0; i < 10; i++)
+                {
+                    var isRunning = true;
+                    while (isRunning)
+                    {
+                        foreach (var f in fList)
+                        {
+                            isRunning = ProcessEx.IsRunning(f);
+                            if (!isRunning)
+                                continue;
+                            i = 0;
+                            break;
+                        }
+                        Thread.Sleep(200);
+                    }
+                    Thread.Sleep(300);
+                }
+
+                Data.DirUnLink(defAppDir, true);
+                Data.DirUnLink(defTmpDir, true);
+                Data.DirUnLink(defDataDir, true);
+
+                if (defaultKeys.Any(Reg.SubKeyExist))
+                    Reg.ExportKeys(regPath, defaultKeys);
+
+                ClearRegistry:
+
+                foreach (var key in defaultKeys)
+                    Reg.RemoveExistSubKey(key);
+
+                for (var i = 0; i < backupKeys.Length; i++)
+                    Reg.MoveSubKey(backupKeys[i], defaultKeys[i].Substring(5));
+
+                if (!Elevation.IsAdministrator && defaultKeys.Any(Reg.SubKeyExist))
+                    Elevation.RestartAsAdministrator("{E429830E-A5A1-4FA0-9D5F-B21964F4698E}");
+            }
+        }
+    }
+}
---
+++ Helper for third party apps/DiscordPortable/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Discord Portable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyProduct("DiscordPortable")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("613a9181-e474-40d0-a983-ddf5679207c5")]
+
+[assembly: AssemblyVersion("1.0.*")]
