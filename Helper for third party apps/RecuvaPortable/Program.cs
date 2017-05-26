68034284f6ae92b4df2419ff7c0c56466420eed2
Si13n7 <si13n7@live.de>
2017-05-26 14:15:28 +0200

    3rd-Party: Add Recuva launcher.

---
+++ Helper for third party apps/RecuvaPortable/Program.cs
@@ -0,0 +1,69 @@
+namespace RecuvaPortable
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading;
+    using Portable;
+    using SilDev;
+
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Log.AllowLogging();
+            bool newInstance;
+            using (new Mutex(true, ProcessEx.CurrentName, out newInstance))
+            {
+                if (!newInstance)
+                    return;
+
+                var appDir = PathEx.Combine(PathEx.LocalDir, "App\\Recuva");
+                if (!Directory.Exists(appDir))
+                    return;
+
+                var appPath = Path.Combine(appDir, "Recuva.exe");
+                var updaterPath = Path.Combine(appDir, "RecuvaUpdater.exe");
+                if (!File.Exists(appPath) || ProcessEx.InstancesCount("Recuva") > 0 || !File.Exists(updaterPath) || ProcessEx.InstancesCount("RecuvaUpdater") > 0)
+                    return;
+
+                CleanUpOld();
+                var dataDir = PathEx.Combine(PathEx.LocalDir, "Data");
+                var fileMap = new Dictionary<string, string>
+                {
+                    {
+                        PathEx.Combine(appDir, "recuva.ini"),
+                        PathEx.Combine(dataDir, "recuva.ini")
+                    }
+                };
+                Helper.FileForwarding(Helper.Options.Start, fileMap);
+
+                var portableDat = Path.Combine(appDir, "portable.dat");
+                if (!File.Exists(portableDat))
+                    File.WriteAllText(portableDat, @"#PORTABLE#");
+
+                Helper.ApplicationStart(updaterPath, "/silent", false);
+                Helper.ApplicationStart(appPath, EnvironmentEx.CommandLine(false), false);
+
+                Helper.FileForwarding(Helper.Options.Exit, fileMap);
+            }
+        }
+
+        private static void CleanUpOld()
+        {
+            var dataDir = PathEx.Combine(PathEx.LocalDir, "Data");
+            var appDir = PathEx.Combine(PathEx.LocalDir, "Recuva");
+            if (!Directory.Exists(appDir))
+                return;
+            var oldCfgPath = Path.Combine(appDir, "recuva.ini");
+            if (File.Exists(oldCfgPath))
+            {
+                if (!Directory.Exists(dataDir))
+                    Directory.CreateDirectory(dataDir);
+                File.Move(oldCfgPath, PathEx.Combine(dataDir, "recuva.ini"));
+            }
+            Directory.Delete(appDir, true);
+        }
+    }
+}
---
+++ Helper for third party apps/RecuvaPortable/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Recuva Portable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyProduct("RecuvaPortable")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af22fec2-e9aa-40f0-a4a7-cf02e6232e08")]
+
+[assembly: AssemblyVersion("1.0.*")]
