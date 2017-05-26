8b39f49d299594ae97fa24e2f6fa8dfcc9ccc9fd
Si13n7 <si13n7@live.de>
2017-05-26 12:50:22 +0200

    3rd-Party: Rework Defraggler launcher.

--- Helper for third party apps/DefragglerPortable/Program.cs
+++ Helper for third party apps/DefragglerPortable/Program.cs
@@ -1,9 +1,10 @@
 namespace DefragglerPortable
 {
     using System;
-    using System.Diagnostics;
+    using System.Collections.Generic;
     using System.IO;
     using System.Threading;
+    using Portable;
     using SilDev;
 
     internal static class Program
@@ -13,27 +14,67 @@ namespace DefragglerPortable
         {
             Log.AllowLogging();
             bool newInstance;
-            using (new Mutex(true, Process.GetCurrentProcess().ProcessName, out newInstance))
-                if (newInstance)
+            using (new Mutex(true, ProcessEx.CurrentName, out newInstance))
+            {
+                if (!newInstance)
+                    return;
+
+                var appDir = PathEx.Combine(PathEx.LocalDir, "App\\Defraggler");
+                if (!Directory.Exists(appDir))
+                    return;
+
+                var appPath = Path.Combine(appDir, "Defraggler.exe");
+                var updaterPath = Path.Combine(appDir, "DefragglerUpdater.exe");
+                if (!File.Exists(appPath) || ProcessEx.InstancesCount("Defraggler") > 0 || !File.Exists(updaterPath) || ProcessEx.InstancesCount("DefragglerUpdater") > 0)
+                    return;
+
+                CleanUpOld();
+                var dataDir = PathEx.Combine(PathEx.LocalDir, "Data");
+                var fileMap = new Dictionary<string, string>
                 {
-                    var rootDir = PathEx.Combine("%CurDir%\\Defraggler");
-                    if (!Directory.Exists(rootDir))
-                        return;
-                    var appPath = Path.Combine(rootDir, "Defraggler.exe");
-                    var updaterPath = Path.Combine(rootDir, "DefragglerUpdater.exe");
-                    if (!File.Exists(appPath) || Process.GetProcessesByName(Path.GetFileNameWithoutExtension(appPath)).Length > 0 ||
-                        !File.Exists(updaterPath) || Process.GetProcessesByName(Path.GetFileNameWithoutExtension(updaterPath)).Length > 0)
-                        return;
-                    var portableDat = Path.Combine(rootDir, "portable.dat");
-                    if (!File.Exists(portableDat))
-                        File.WriteAllText(portableDat, @"#PORTABLE#");
-                    using (var p = ProcessEx.Start(updaterPath, "/silent", true, false))
-                        if (!p?.HasExited == true)
-                            p?.WaitForExit();
-                    using (var p = ProcessEx.Start(appPath, EnvironmentEx.CommandLine(false), true, false))
-                        if (!p?.HasExited == true)
-                            p?.WaitForExit();
-                }
+                    {
+                        PathEx.Combine(appDir, "defraggler.ini"),
+                        PathEx.Combine(dataDir, "defraggler.ini")
+                    },
+                    {
+                        PathEx.Combine(appDir, "statistics.bin"),
+                        PathEx.Combine(dataDir, "statistics.bin")
+                    }
+                };
+                Helper.FileForwarding(Helper.Options.Start, fileMap);
+
+                var portableDat = Path.Combine(appDir, "portable.dat");
+                if (!File.Exists(portableDat))
+                    File.WriteAllText(portableDat, @"#PORTABLE#");
+
+                Helper.ApplicationStart(updaterPath, "/silent", false);
+                Helper.ApplicationStart(appPath, EnvironmentEx.CommandLine(false), false);
+
+                Helper.FileForwarding(Helper.Options.Exit, fileMap);
+            }
+        }
+
+        private static void CleanUpOld()
+        {
+            var dataDir = PathEx.Combine(PathEx.LocalDir, "Data");
+            var appDir = PathEx.Combine(PathEx.LocalDir, "Defraggler");
+            if (!Directory.Exists(appDir))
+                return;
+            var oldCfgPath = Path.Combine(appDir, "defraggler.ini");
+            if (File.Exists(oldCfgPath))
+            {
+                if (!Directory.Exists(dataDir))
+                    Directory.CreateDirectory(dataDir);
+                File.Move(oldCfgPath, PathEx.Combine(dataDir, "defraggler.ini"));
+            }
+            var oldStsPath = Path.Combine(appDir, "statistics.bin");
+            if (File.Exists(oldStsPath))
+            {
+                if (!Directory.Exists(dataDir))
+                    Directory.CreateDirectory(dataDir);
+                File.Move(oldStsPath, PathEx.Combine(dataDir, "statistics.bin"));
+            }
+            Directory.Delete(appDir, true);
         }
     }
 }
