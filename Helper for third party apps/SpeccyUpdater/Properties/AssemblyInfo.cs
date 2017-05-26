02f92dcf764707e98d14db620b6c21b9c351af27
Si13n7 <si13n7@live.de>
2017-05-26 13:33:07 +0200

    3rd-Party: Add Speccy updater.

---
+++ Helper for third party apps/SpeccyUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpeccyUpdater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyProduct("SpeccyUpdater")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16a1d9e7-0a7c-45ed-a025-15dc9736dcb4")]
+
+[assembly: AssemblyVersion("1.0.*")]
