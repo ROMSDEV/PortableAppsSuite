ca73ce5f1b10b4cb07c35ea438b2559ff33226a7
Si13n7 <si13n7@live.de>
2017-05-26 12:12:34 +0200

    3rd-Party: Rework CCleaner launcher.

--- Helper for third party apps/CCleanerPortable/Program.cs
+++ Helper for third party apps/CCleanerPortable/Program.cs
@@ -1,9 +1,10 @@
 namespace CCleanerPortable
 {
     using System;
-    using System.Diagnostics;
+    using System.Collections.Generic;
     using System.IO;
     using System.Threading;
+    using Portable;
     using SilDev;
 
     internal static class Program
@@ -13,27 +14,56 @@ namespace CCleanerPortable
         {
             Log.AllowLogging();
             bool newInstance;
-            using (new Mutex(true, Process.GetCurrentProcess().ProcessName, out newInstance))
-                if (newInstance)
+            using (new Mutex(true, ProcessEx.CurrentName, out newInstance))
+            {
+                if (!newInstance)
+                    return;
+
+                var appDir = PathEx.Combine(PathEx.LocalDir, "App\\CCleaner");
+                if (!Directory.Exists(appDir))
+                    return;
+
+                var appPath = Path.Combine(appDir, "CCleaner.exe");
+                var updaterPath = Path.Combine(appDir, "CCleanerUpdater.exe");
+                if (!File.Exists(appPath) || ProcessEx.InstancesCount("CCleaner") > 0 || !File.Exists(updaterPath) || ProcessEx.InstancesCount("CCleanerUpdater") > 0)
+                    return;
+
+                CleanUpOld();
+                var dataDir = PathEx.Combine(PathEx.LocalDir, "Data");
+                var fileMap = new Dictionary<string, string>
                 {
-                    var rootDir = PathEx.Combine("%CurDir%\\CCleaner");
-                    if (!Directory.Exists(rootDir))
-                        return;
-                    var appPath = Path.Combine(rootDir, "CCleaner.exe");
-                    var updaterPath = Path.Combine(rootDir, "CCleanerUpdater.exe");
-                    if (!File.Exists(appPath) || Process.GetProcessesByName(Path.GetFileNameWithoutExtension(appPath)).Length > 0 ||
-                        !File.Exists(updaterPath) || Process.GetProcessesByName(Path.GetFileNameWithoutExtension(updaterPath)).Length > 0)
-                        return;
-                    var portableDat = Path.Combine(rootDir, "portable.dat");
-                    if (!File.Exists(portableDat))
-                        File.WriteAllText(portableDat, @"#PORTABLE#");
-                    using (var p = ProcessEx.Start(updaterPath, "/silent", true, false))
-                        if (!p?.HasExited == true)
-                            p?.WaitForExit();
-                    using (var p = ProcessEx.Start(appPath, EnvironmentEx.CommandLine(false), true, false))
-                        if (!p?.HasExited == true)
-                            p?.WaitForExit();
-                }
+                    {
+                        PathEx.Combine(appDir, "ccleaner.ini"),
+                        PathEx.Combine(dataDir, "ccleaner.ini")
+                    }
+                };
+                Helper.FileForwarding(Helper.Options.Start, fileMap);
+
+                var portableDat = Path.Combine(appDir, "portable.dat");
+                if (!File.Exists(portableDat))
+                    File.WriteAllText(portableDat, @"#PORTABLE#");
+
+                Helper.ApplicationStart(updaterPath, "/silent", false);
+                Helper.ApplicationStart(appPath, EnvironmentEx.CommandLine(false), false);
+
+                Helper.FileForwarding(Helper.Options.Exit, fileMap);
+            }
+        }
+
+        private static void CleanUpOld()
+        {
+            var dataDir = PathEx.Combine(PathEx.LocalDir, "Data");
+            var appDir = PathEx.Combine(PathEx.LocalDir, "CCleaner");
+            if (!Directory.Exists(appDir))
+                return;
+            var oldCfgPath = Path.Combine(appDir, "ccleaner.ini");
+            if (File.Exists(oldCfgPath))
+            {
+                if (!Directory.Exists(dataDir))
+                    Directory.CreateDirectory(dataDir);
+                File.Move(oldCfgPath, PathEx.Combine(dataDir, "ccleaner.ini"));
+            }
+            Directory.Delete(appDir, true);
         }
     }
 }
