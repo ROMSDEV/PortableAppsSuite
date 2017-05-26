5ba9041456da8b9ea3dbb84f3e87fc12761597fb
Si13n7 <si13n7@live.de>
2017-05-26 14:15:47 +0200

    3rd-Party: Add Recuva updater.

---
+++ Helper for third party apps/RecuvaUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RecuvaUpdater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyProduct("RecuvaUpdater")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("605f89d3-0154-481b-a41e-996bd602d3a1")]
+
+[assembly: AssemblyVersion("1.0.*")]
