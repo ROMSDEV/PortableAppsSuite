e8452e8aa998d67ffde8a1c5b4538ce3ee8edbb6
Si13n7 <si13n7@live.de>
2017-06-01 21:18:23 +0200

    3rd-Party: Add HeidiSQL updater.

---
+++ Helper for third party apps/HeidiSQLUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HeidiSQLUpdater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyProduct("HeidiSQLUpdater")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08397f6c-512d-4ca5-a41a-b62e5faa0fbd")]
+
+[assembly: AssemblyVersion("1.0.*")]
