65d823d27b8aa83d0797fcd175060e47a16541dd
Si13n7 <si13n7@live.de>
2017-06-01 23:49:29 +0200

    3rd-Party: Add YoloMouse updater.

---
+++ Helper for third party apps/YoloMouseUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if x86
+[assembly: AssemblyTitle("YoloMouseUpdater")]
+[assembly: AssemblyProduct("YoloMouseUpdater")]
+#else
+[assembly: AssemblyTitle("YoloMouse64Updater")]
+[assembly: AssemblyProduct("YoloMouse64Updater")]
+#endif
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d11b001a-08c9-421b-bcaa-b7c4809ce734")]
+
+[assembly: AssemblyVersion("1.0.*")]
