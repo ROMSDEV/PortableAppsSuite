612c4b081e3a1c8b75c3af54fd4e3c1d44167c10
Si13n7 <si13n7@live.de>
2017-06-01 14:41:00 +0200

    3rd-Party: Add NTLite updater.

---
+++ Helper for third party apps/NTLiteUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if x86
+[assembly: AssemblyTitle("NTLiteUpdater")]
+[assembly: AssemblyProduct("NTLiteUpdater")]
+#else
+[assembly: AssemblyTitle("NTLite64Updater")]
+[assembly: AssemblyProduct("NTLite64Updater")]
+#endif
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d7783de-fdaf-42c9-9499-bc34ba1555ab")]
+
+[assembly: AssemblyVersion("1.0.*")]
