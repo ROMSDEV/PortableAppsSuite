d14c2825c92ac1f4cca618872680651280b3a175
Si13n7 <si13n7@live.de>
2017-06-02 15:42:27 +0200

    3rd-Party: Rework AIDA64 launcher.

--- Helper for third party apps/AIDA64Portable/Program.cs
+++ Helper for third party apps/AIDA64Portable/Program.cs
@@ -1,9 +1,11 @@
 namespace AIDA64Portable
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.IO;
     using System.Threading;
+    using Portable;
     using SilDev;
 
     internal static class Program
@@ -14,29 +16,68 @@ namespace AIDA64Portable
             Log.AllowLogging();
             bool newInstance;
             using (new Mutex(true, Process.GetCurrentProcess().ProcessName, out newInstance))
-                if (newInstance)
+            {
+                if (!newInstance)
+                    return;
+
+                var appPath = PathEx.Combine(PathEx.LocalDir, "App\\AIDA64\\aida64.exe");
+                if (!File.Exists(appPath) || ProcessEx.InstancesCount(Path.GetFileNameWithoutExtension(appPath)) > 0)
+                    return;
+
+                CleanUpHelper();
+
+                const string iniPath = "%CurDir%\\Data\\aida64.ini";
+                var iniMap = new Dictionary<string, Dictionary<string, string>>
                 {
-                    var appPath = PathEx.Combine("%CurDir%\\AIDA64\\aida64.exe");
-                    if (!File.Exists(appPath) || Process.GetProcessesByName("aida64").Length > 0)
-                        return;
-                    var regBackup = false;
-                    const string regKey = "HKEY_CURRENT_USER\\Software\\FinalWire";
-                    if (Reg.SubKeyExists(regKey))
-                        if (string.IsNullOrWhiteSpace(Reg.ReadString(regKey, "Portable App")))
+                    {
+                        "Generic", new Dictionary<string, string>
                         {
-                            Reg.MoveSubKey(regKey, "HKEY_CURRENT_USER\\Software\\SI13N7-BACKUP: FinalWire");
-                            regBackup = true;
+                            {
+                                "NoRegistry",
+                                "1"
+                            },
+                            {
+                                "NetUpdateFreq",
+                                "1"
+                            },
+                            {
+                                "ReportFileFolder",
+                                PathEx.Combine(PathEx.LocalDir, "Data\\Reports")
+                            }
                         }
-                    Reg.ImportFile(PathEx.Combine("%CurDir%\\settings.reg"));
-                    Reg.Write(regKey, "Portable App", "True");
-                    using (var p = ProcessEx.Start(appPath, true, false))
-                        if (!p?.HasExited == true)
-                            p?.WaitForExit();
-                    Reg.ExportKeys(PathEx.Combine("%CurDir%\\settings.reg"), regKey);
-                    Reg.RemoveSubKey("HKEY_CURRENT_USER\\Software\\FinalWire");
-                    if (regBackup)
-                        Reg.MoveSubKey("HKEY_CURRENT_USER\\Software\\SI13N7-BACKUP: FinalWire", "HKEY_CURRENT_USER\\Software\\FinalWire");
-                }
+                    }
+                };
+
+                var fileMap = new Dictionary<string, string>
+                {
+                    {
+                        "%CurDir%\\App\\AIDA64\\aida64.ini",
+                        "%CurDir%\\Data\\aida64.ini"
+                    },
+                    {
+                        "%CurDir%\\App\\AIDA64\\aida64.reg.ini",
+                        "%CurDir%\\Data\\aida64.reg.ini"
+                    }
+                };
+
+                Helper.ConfigOverwrite(iniMap, iniPath);
+
+                Helper.FileForwarding(Helper.Options.Start, fileMap, true);
+
+                Helper.ApplicationStart(appPath, EnvironmentEx.CommandLine(false), false);
+
+                Helper.FileForwarding(Helper.Options.Exit, fileMap, true);
+            }
+        }
+
+        private static void CleanUpHelper()
+        {
+            var appDir = PathEx.Combine(PathEx.LocalDir, "AIDA64");
+            if (Directory.Exists(appDir))
+                Directory.Delete(appDir, true);
+            var settingsPath = PathEx.Combine(PathEx.LocalDir, "settings.reg");
+            if (File.Exists(settingsPath))
+                File.Delete(settingsPath);
         }
     }
 }
