b766f54105db2c3a2ecb832b897869406a2d4e95
Si13n7 <si13n7@live.de>
2017-06-03 04:20:54 +0200

    3rd-Party: Add HWMonitor launcher.

---
+++ Helper for third party apps/HWMonitorPortable/Program.cs
@@ -0,0 +1,85 @@
+namespace HWMonitorPortable
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading;
+    using Portable;
+    using Properties;
+    using SilDev;
+
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Log.AllowLogging();
+            bool newInstance;
+            using (new Mutex(true, ProcessEx.CurrentName, out newInstance))
+            {
+                if (!newInstance)
+                    return;
+
+                CleanUpHelper();
+
+                var appDir = PathEx.Combine(PathEx.LocalDir, "App\\hwmon");
+                if (!Directory.Exists(appDir))
+                    return;
+
+                var appPath = Path.Combine(appDir, Environment.CommandLine.ContainsEx("/Tray") ? "HwMonTray.exe" : (Environment.Is64BitOperatingSystem ? "HWMonitor_x64.exe" : "HWMonitor_x32.exe"));
+                var updaterPath = Path.Combine(appDir, "HWMonitorUpdater.exe");
+                if (ProcessEx.InstancesCount(Path.GetFileNameWithoutExtension(appPath)) > 0 || !File.Exists(updaterPath) || ProcessEx.InstancesCount(Path.GetFileNameWithoutExtension(updaterPath)) > 0)
+                    return;
+
+                Helper.ApplicationStart(updaterPath, "/silent", null);
+                if (!File.Exists(appPath))
+                {
+                    var updIniPath = Path.ChangeExtension(updaterPath, ".ini");
+                    if (File.Exists(updIniPath))
+                        File.Delete(updIniPath);
+                    return;
+                }
+
+                const string iniPath = "%CurDir%\\Data\\HWMonitorW.ini";
+                var iniMap = new Dictionary<string, Dictionary<string, string>>
+                {
+                    {
+                        "HWMonitor", new Dictionary<string, string>
+                        {
+                            {
+                                "UPDATES",
+                                "0"
+                            }
+                        }
+                    }
+                };
+
+                var fileMap = new Dictionary<string, string>
+                {
+                    {
+                        "%CurDir%\\App\\hwmon\\HWMonitorW.ini",
+                        "%CurDir%\\Data\\HWMonitorW.ini"
+                    }
+                };
+
+                Helper.ConfigOverwrite(iniMap, iniPath);
+
+                Helper.FileForwarding(Helper.Options.Start, fileMap, true);
+
+                Helper.ApplicationStart(appPath, EnvironmentEx.CommandLine(false), false);
+
+                Helper.FileForwarding(Helper.Options.Exit, fileMap, true);
+            }
+        }
+
+        private static void CleanUpHelper()
+        {
+            foreach (var file in Resources.FileList.SplitNewLine())
+            {
+                var path = PathEx.Combine(PathEx.LocalDir, file);
+                if (File.Exists(path))
+                    File.Delete(path);
+            }
+        }
+    }
+}
---
+++ Helper for third party apps/HWMonitorPortable/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HWMonitor Portable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyProduct("HWMonitorPortable")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11d9b8ac-b2ff-4da6-a72a-2056ca3f7558")]
+
+[assembly: AssemblyVersion("1.0.*")]
