ebdb7190b0635fd12814f85a8927bb6612a1b3e7
Si13n7 <si13n7@live.de>
2017-06-03 04:20:39 +0200

    3rd-Party: Add HWMonitor updater.

---
+++ Helper for third party apps/HWMonitorUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HWMonitorUpdater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyProduct("HWMonitorUpdater")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a01e51c-f8ab-41ce-909e-dd2ea3a61e63")]
+
+[assembly: AssemblyVersion("1.0.*")]
