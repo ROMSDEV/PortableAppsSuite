91e7106f578cf28f9399a119db3b46893c8526ee
Si13n7 <si13n7@live.de>
2017-06-03 17:08:17 +0200

    3rd-Party: Add FurMark updater.

---
+++ Helper for third party apps/FurMarkUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FurMarkUpdater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyProduct("FurMarkUpdater")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d7783de-fdaf-42c9-9499-bc34ba1555ab")]
+
+[assembly: AssemblyVersion("1.0.*")]
