a3a24b543c43034de04547843a4dbe9e46bdb524
Si13n7 <si13n7@live.de>
2017-06-04 06:15:18 +0200

    3rd-Party: Add Gyazo updater.

---
+++ Helper for third party apps/GyazoUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GyazoUpdater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyProduct("GyazoUpdater")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a7d8646-6004-42fd-b3fc-a7d4e685dfd0")]
+
+[assembly: AssemblyVersion("17.6.4.*")]
