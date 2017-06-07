3584ede72ded3d9d8d1e59d04a35ec09f471ab8b
Si13n7 <si13n7@live.de>
2017-06-07 18:27:31 +0200

    3rd-Party: Rework HLSW launcher.

--- Helper for third party apps/HLSWPortable/Program.cs
+++ Helper for third party apps/HLSWPortable/Program.cs
@@ -2,10 +2,12 @@ namespace HLSWPortable
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.IO;
+    using System.Linq;
     using System.Text;
     using System.Threading;
+    using Microsoft.Win32;
+    using Portable;
     using SilDev;
 
     internal static class Program
@@ -19,20 +21,17 @@ namespace HLSWPortable
             {
                 if (!newInstance)
                     return;
-                var appPath = PathEx.Combine("%CurDir%\\App\\HLSW\\hlsw.exe");
-                if (!File.Exists(appPath) || Process.GetProcessesByName(Path.GetFileNameWithoutExtension(appPath)).Length > 0)
-                    return;
 
-                string appBackupPath = $"{appPath}.SI13N7-BACKUP";
-                string oldAppBackupPath = $"{appPath}.BACKUP";
-                if (File.Exists(oldAppBackupPath) && !File.Exists(appBackupPath))
-                    File.Move(oldAppBackupPath, appBackupPath);
+                var appPath = PathEx.Combine(PathEx.LocalDir, "App\\HLSW\\hlsw.exe");
+                if (!File.Exists(appPath) || ProcessEx.IsRunning(Path.GetFileNameWithoutExtension(appPath)))
+                    return;
 
                 try
                 {
-                    if (!File.Exists(appBackupPath) && Crypto.EncryptFileToMd5(appPath).EqualsEx("1715a84deb7c6c3f35dc8acd96833ef9"))
+                    string unpachedAppPath = $"{appPath}.SI13N7-BACKUP";
+                    if (!File.Exists(unpachedAppPath) && Crypto.EncryptFileToMd5(appPath).EqualsEx("1715a84deb7c6c3f35dc8acd96833ef9"))
                     {
-                        File.Copy(appPath, appBackupPath);
+                        File.Copy(appPath, unpachedAppPath);
                         using (var bw = new BinaryWriter(File.Open(appPath, FileMode.Open)))
                         {
                             bw.BaseStream.Position = 0x439654L;
@@ -45,43 +44,52 @@ namespace HLSWPortable
                     Log.Write(ex);
                 }
 
-                if (Crypto.EncryptFileToMd5(appPath) != "8f4fcb7555b8a5cab46ca99750346321")
+                if (!Crypto.EncryptFileToMd5(appPath).EqualsEx("8f4fcb7555b8a5cab46ca99750346321"))
                     return;
 
-                var cfgPath = PathEx.Combine("%CurDir%\\Data\\Cfg");
-                if (!Directory.Exists(cfgPath))
-                    Directory.CreateDirectory(cfgPath);
+                Helper.RedistHandling(Helper.Options.Start, EnvironmentEx.RedistPack.VC2008_x86);
 
-                if (!Reg.EntryExists("HKCU\\Software\\HLSW", "Portable App"))
-                    Reg.MoveSubKey("HKCU\\Software\\HLSW", "HKCU\\Software\\SI13N7-BACKUP: HLSW");
+                var regKeys = new[]
+                {
+                    "HKCU\\Software\\HLSW",
+                    "HKCU\\Software\\HLSW\\Management"
+                };
 
-                var settingsPath = PathEx.Combine("%CurDir%\\Data\\settings.reg");
-                if (File.Exists(settingsPath))
-                    Reg.ImportFile(settingsPath);
-                Reg.Write("HKCU\\Software\\HLSW", "Portable App", "True");
-                Reg.Write("HKCU\\Software\\HLSW\\Management", "AutoLogin", 1);
-                Reg.Write("HKCU\\Software\\HLSW\\Management", "LoginOnStartup", 0);
-                Reg.Write("HKCU\\Software\\HLSW\\Management", "Offline", 1);
-                Reg.Write("HKCU\\Software\\HLSW\\Management", "Offline2", 1);
+                var dataPath = PathEx.Combine(PathEx.LocalDir, "Data\\Cfg");
+                if (!Directory.Exists(dataPath))
+                    Directory.CreateDirectory(dataPath);
 
-                using (var p = ProcessEx.Start(appPath, true, false))
-                    if (!p?.HasExited == true)
-                        p?.WaitForExit();
+                var firstKey = regKeys.FirstOrDefault();
+                if (!string.IsNullOrEmpty(firstKey))
+                    Helper.RegForwarding(Helper.Options.Start, firstKey);
 
-                Reg.ExportKeys(settingsPath, "HKCU\\Software\\HLSW");
-                Reg.RemoveSubKey("HKCU\\Software\\HLSW");
-                Reg.MoveSubKey("HKCU\\Software\\SI13N7-BACKUP: HLSW", "HKCU\\Software\\HLSW");
+                var lastKey = regKeys.LastOrDefault();
+                if (!string.IsNullOrEmpty(lastKey))
+                {
+                    Reg.Write(lastKey, "AutoLogin", 1, RegistryValueKind.DWord);
+                    Reg.Write(lastKey, "LoginOnStartup", 0, RegistryValueKind.DWord);
+                    Reg.Write(lastKey, "Offline", 1, RegistryValueKind.DWord);
+                    Reg.Write(lastKey, "Offline2", 1, RegistryValueKind.DWord);
+                }
 
-                if (!Elevation.IsAdministrator)
-                    return;
-                var regFileCon = new List<string> { "Windows Registry Editor Version 5.00", string.Empty };
+                Helper.ApplicationStart(appPath, EnvironmentEx.CommandLine(false), false);
+
+                if (!string.IsNullOrEmpty(firstKey))
+                    Helper.RegForwarding(Helper.Options.Exit, firstKey);
+
+                var regSecureMap = new Dictionary<string, Dictionary<string, string>>
+                {
+                    { "[-HKEY_CLASSES_ROOT\\.sslf]", null },
+                    { "[-HKEY_CLASSES_ROOT\\hlsw]", null },
+                    { "[-HKEY_CLASSES_ROOT\\HLSW Server List]", null }
+                };
                 for (var i = 0; i < 34; i++)
-                    regFileCon.Add($"[-HKEY_CLASSES_ROOT\\.sl{(i < 10 ? $"0{i}" : i.ToString())}]");
-                regFileCon.Add("[-HKEY_CLASSES_ROOT\\.sslf]");
-                regFileCon.Add("[-HKEY_CLASSES_ROOT\\hlsw]");
-                regFileCon.Add("[-HKEY_CLASSES_ROOT\\HLSW Server List]");
-                regFileCon.Add("[-HKEY_CURRENT_USER\\Software\\HLSW]");
-                Reg.ImportFile(regFileCon.ToArray(), true);
+                    regSecureMap.Add($"[-HKEY_CLASSES_ROOT\\.sl{(i < 10 ? $"0{i}" : i.ToString())}]", null);
+
+                if (regSecureMap.Keys.Any(Reg.SubKeyExists))
+                    Helper.RegSecureOverwrite(regSecureMap, true);
+
+                Helper.RedistHandling(Helper.Options.Exit, EnvironmentEx.RedistPack.VC2008_x86);
             }
         }
     }
