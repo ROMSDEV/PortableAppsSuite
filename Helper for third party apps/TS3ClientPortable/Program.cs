1635864fd1511f6599b282919c056288a8eafc09
Si13n7 <si13n7@live.de>
2017-06-11 04:15:49 +0200

    3rd-Party: Rework TS3 launcher.

--- Helper for third party apps/TS3ClientPortable/Program.cs
+++ Helper for third party apps/TS3ClientPortable/Program.cs
@@ -1,10 +1,14 @@
 namespace TS3ClientPortable
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Threading;
+    using Microsoft.Win32;
+    using Portable;
+    using Properties;
     using SilDev;
 
     internal static class Program
@@ -29,154 +33,177 @@ namespace TS3ClientPortable
 
                 var appDir = PathEx.Combine(PathEx.LocalDir, "App\\ts3_x86");
                 var appPath = PathEx.Combine(appDir, "ts3client_win32.exe");
+                const string appName = "ts3client_win32";
 #else
                 var appDir = PathEx.Combine(PathEx.LocalDir, "App\\ts3_x64");
                 var appPath = PathEx.Combine(appDir, "ts3client_win64.exe");
+                const string appName = "ts3client_win64";
 #endif
 
-                if (!File.Exists(appPath) || Process.GetProcessesByName(Path.GetFileNameWithoutExtension(appPath)).Length > 0)
+                var updPath = PathEx.Combine(appDir, "update.exe");
+                if (!File.Exists(updPath) || ProcessEx.IsRunning(appName))
                     return;
 
                 var iniPath = Path.ChangeExtension(PathEx.LocalPath, ".ini");
-
-                var waitForNetwork = Ini.Read("Settings", "WaitForNetwork", iniPath);
-                int ms;
-                if (int.TryParse(waitForNetwork, out ms))
-                    try
+                if (string.IsNullOrEmpty(iniPath))
+                    return;
+                if (!File.Exists(iniPath))
+                {
+                    var iniContent = new[]
                     {
-                        if (ms > 0)
-                            for (var i = 0; i < ms; i++)
-                            {
-                                Thread.Sleep(1);
-                                if (NetEx.InternetIsAvailable())
-                                    break;
-                            }
-                    }
-                    catch (Exception ex)
+                        "[Settings]",
+                        "",
+                        "; True to hide TeamSpeak on the Windows taskbar; otherwise, False.",
+                        "HideInTaskBar=False",
+                        "",
+                        "; Time (in milliseconds) to wait for a network connection before TeamSpeak is started.",
+                        "; This option can be useful for the Windows autorun feature. If TeamSpeak is started",
+                        "; before the network connection is available, the connection is failed and no replay",
+                        "; occurs.",
+                        "WaitForNetwork=0",
+                        "",
+                        "; Sets the window state of TeamSpeak. Options: Maximized, Minimized or Normal.",
+                        "WinState=Normal"
+                    };
+                    File.WriteAllLines(iniPath, iniContent);
+                }
+
+                var time = Ini.Read("Settings", "WaitForNetwork", 0, iniPath);
+                if (time > 300000)
+                    time = 300000;
+                if (time.IsBetween(1, 300000))
+                    for (var i = 0; i < time; i++)
                     {
-                        Log.Write(ex);
+                        if (NetEx.InternetIsAvailable())
+                            break;
+                        Thread.Sleep(1);
                     }
 
-                var dataDirMap = new[,]
+                var dirMap = new Dictionary<string, string>
                 {
                     {
                         PathEx.Combine(appDir, "config"),
-                        PathEx.Combine("%CurDir%\\Data")
+                        "%CurDir%\\Data"
                     },
                     {
-                        PathEx.Combine("%UserProfile%\\.QtWebEngineProcess"),
-                        PathEx.Combine("%CurDir%\\Data\\.QtWebEngineProcess")
+                        "%LocalAppData%\\TeamSpeak 3",
+                        "%CurDir%\\Data\\Temp"
                     },
                     {
-                        PathEx.Combine("%UserProfile%\\.TeamSpeak 3"),
-                        PathEx.Combine("%CurDir%\\Data\\.TeamSpeak 3")
+                        "%UserProfile%\\.QtWebEngineProcess",
+                        "%CurDir%\\Data\\Temp\\.QtWebEngineProcess"
+                    },
+                    {
+                        "%UserProfile%\\.TeamSpeak 3",
+                        "%CurDir%\\Data\\Temp\\.TeamSpeak 3"
                     }
                 };
-                for (var i = 0; i < dataDirMap.GetLength(0); i++)
-                {
-                    if (!Directory.Exists(dataDirMap[i, 1]))
-                        Directory.CreateDirectory(dataDirMap[i, 1]);
-                    Data.DirLink(dataDirMap[i, 0], dataDirMap[i, 1], true);
-                }
+                Helper.DirectoryForwarding(Helper.Options.Start, dirMap);
 
-                var regKeyMap = new [,]
+                var regKeys = new[]
                 {
-                    {
-                        "HKCU\\Software\\TeamSpeak 3 Client",
-                        "HKCU\\Software\\SI13N7-BACKUP: TeamSpeak 3 Client"
-                    },
-                    {
-                        "HKLM\\SOFTWARE\\TeamSpeak 3 Client",
-                        "HKLM\\SOFTWARE\\SI13N7-BACKUP: TeamSpeak 3 Client"
-                    }
+                    "HKCU\\Software\\TeamSpeak 3 Client",
+                    "HKCR\\.ts3_addon",
+                    "HKCR\\.ts3_iconpack",
+                    "HKCR\\.ts3_plugin",
+                    "HKCR\\.ts3_soundpack",
+                    "HKCR\\.ts3_style",
+                    "HKCR\\.ts3_translation",
+                    "HKCR\\ts3addon"
                 };
-                for (var i = 0; i < regKeyMap.GetLength(0); i++)
+                var firstKey = regKeys.LastOrDefault();
+                Helper.RegForwarding(Helper.Options.Start, firstKey);
+                Reg.Write(firstKey, null, appDir);
+                Reg.Write(firstKey, "ConfigLocation", 1, RegistryValueKind.String);
+
+                Process appProcess = null;
+                if (File.Exists(appPath))
+                    appProcess = ProcessEx.Start(appPath, EnvironmentEx.CommandLine(false), Elevation.IsAdministrator, false);
+                else
+                    ProcessEx.Start(updPath, Elevation.IsAdministrator, false)?.WaitForExit();
+
+                var wrapper = PathEx.Combine(appDir, "createfileassoc.exe");
+                var hide = Ini.Read("Settings", "HideInTaskBar", false, iniPath);
+                var state = Ini.Read("Settings", "WinState", iniPath);
+                var enable = state.EqualsEx("Maximized", "Minimized", iniPath);
+
+                if (appProcess == null)
+                    goto Second;
+
+                First:
+                while (!appProcess.HasExited)
                 {
-                    if (Reg.EntryExists(regKeyMap[i, 0], "Portable App"))
+                    appProcess.WaitForExit(1000);
+
+                    if (File.Exists(wrapper) && !Crypto.EncryptFileToMd5(wrapper).EqualsEx("cdbe3628ca898a852502c8ae897e5a54"))
+                        try
+                        {
+                            File.WriteAllBytes(wrapper, Resources.EmptyWrapper);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Write(ex);
+                        }
+
+                    if (!hide && !enable)
                         continue;
-                    if (Reg.SubKeyExists(regKeyMap[i, 0]))
-                        Reg.MoveSubKey(regKeyMap[i, 0], regKeyMap[i, 1]);
-                    Reg.Write(regKeyMap[i, 0], "Portable App", "True");
-                }
 
-                var runningProcess = ProcessEx.Start(appPath, false, false);
+                    var hWnd = WinApi.FindWindowByCaption("TeamSpeak 3");
+                    if (hWnd == IntPtr.Zero)
+                        continue;
 
-                var winState = Ini.Read("Settings", "WinState", iniPath);
-                var winStateEnabled = winState.EqualsEx("Minimized", "Min", "Maximized", "Max", iniPath);
-                var hideInTaskBar = Ini.Read("Settings", "HideInTaskBar", false, iniPath);
-
-                Restart:
-                for (var i = 0; i < 10; i++)
-                {
-                    while (!runningProcess?.HasExited == true)
+                    if (hide)
                     {
-                        runningProcess?.WaitForExit(1000);
-                        if (!winStateEnabled && !hideInTaskBar)
-                            continue;
-                        var hWnd = WinApi.FindWindowByCaption("TeamSpeak 3");
-                        if (hWnd == IntPtr.Zero)
-                            continue;
-                        if (winState.EqualsEx("Minimized", "Min"))
-                        {
-                            winStateEnabled = false;
-                            WinApi.UnsafeNativeMethods.ShowWindowAsync(hWnd, WinApi.ShowWindowFunc.SW_SHOWMINIMIZED);
-                        }
-                        else if (winState.EqualsEx("Maximized", "Max"))
-                        {
-                            winStateEnabled = false;
-                            WinApi.UnsafeNativeMethods.ShowWindowAsync(hWnd, WinApi.ShowWindowFunc.SW_SHOWMAXIMIZED);
-                        }
-                        if (!hideInTaskBar)
-                            continue;
-                        hideInTaskBar = false;
+                        hide = false;
                         TaskBar.DeleteTab(hWnd);
                     }
 
-                    bool isRunning;
-                    do
-                    {
-                        isRunning = ProcessEx.IsRunning(appPath);
-                        Thread.Sleep(200);
-                    }
-                    while (isRunning);
+                    if (!enable)
+                        continue;
+                    enable = false;
+                    WinApi.UnsafeNativeMethods.ShowWindowAsync(hWnd, state.EqualsEx("Minimized") ? WinApi.ShowWindowFunc.SW_SHOWMINIMIZED : WinApi.ShowWindowFunc.SW_SHOWMAXIMIZED);
+                }
+                while (ProcessEx.IsRunning(appName))
+                    Thread.Sleep(200);
 
-                    IntPtr updWindPtr;
-                    do
-                    {
-                        updWindPtr = WinApi.FindWindowByCaption("TeamSpeak 3 Client Update");
+                Second:
+                for (var i = 0; i < 10; i++)
+                {
+                    while (ProcessEx.IsRunning(updPath) || WinApi.FindWindowByCaption("TeamSpeak 3 Client Update") != IntPtr.Zero)
                         Thread.Sleep(200);
-                    }
-                    while (updWindPtr != IntPtr.Zero);
-
-                    if (ProcessEx.IsRunning(appPath))
+                    if (ProcessEx.IsRunning(appName))
                     {
-                        var processes = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(appPath));
-                        if (processes.Length == 0)
+                        appProcess = Process.GetProcessesByName(appName).FirstOrDefault();
+                        if (appProcess == null)
                             break;
-
-                        runningProcess = processes.First();
-
-                        winStateEnabled = winState.StartsWithEx("Min", "Max");
-                        hideInTaskBar = Ini.Read("Settings", "HideInTaskBar", false, iniPath);
-
-                        goto Restart;
+                        hide = Ini.Read("Settings", "HideInTaskBar", false, iniPath);
+                        enable = state.EqualsEx("Maximized", "Minimized", iniPath);
+                        goto First;
                     }
-
                     Thread.Sleep(250);
                 }
 
-                for (var i = 0; i < dataDirMap.GetLength(0); i++)
-                    Data.DirUnLink(dataDirMap[i, 0], true);
+                Helper.DirectoryForwarding(Helper.Options.Exit, dirMap);
 
-                for (var i = 0; i < regKeyMap.GetLength(0); i++)
+                Helper.RegForwarding(Helper.Options.Exit, firstKey);
+
+                var rootKeys = regKeys.Skip(1).ToArray();
+                if (rootKeys.Any(Reg.SubKeyExists))
                 {
-                    Reg.RemoveSubKey(regKeyMap[i, 0]);
-                    Reg.MoveSubKey(regKeyMap[i, 1], regKeyMap[i, 0]);
+                    var regSecureMap = rootKeys.ToDictionary<string, string, Dictionary<string, string>>(x => $"-{x}", x => null);
+                    Helper.RegSecureOverwrite(regSecureMap, true);
                 }
 
-                var owInstallerPath = PathEx.Combine(appDir, "OverwolfTeamSpeakInstaller.exe");
-                if (File.Exists(owInstallerPath))
-                    File.Delete(owInstallerPath);
+                try
+                {
+                    var owInstallerPath = Path.Combine(appDir, "OverwolfTeamSpeakInstaller.exe");
+                    if (File.Exists(owInstallerPath))
+                        File.Delete(owInstallerPath);
+                }
+                catch (Exception ex)
+                {
+                    Log.Write(ex);
+                }
             }
         }
     }
