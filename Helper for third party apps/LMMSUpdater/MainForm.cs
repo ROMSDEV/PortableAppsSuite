1a591a2905e2b8aaf066a297c39097187b63fcb6
Si13n7 <si13n7@live.de>
2017-06-13 01:49:48 +0200

    3rd-Party: Add LMMS updater.

---
+++ Helper for third party apps/LMMSUpdater/MainForm.cs
@@ -0,0 +1,213 @@
+namespace AppUpdater
+{
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System.Windows.Forms;
+    using Properties;
+    using SilDev;
+    using SilDev.Forms;
+
+    public partial class MainForm : Form
+    {
+        private readonly NetEx.AsyncTransfer _transfer = new NetEx.AsyncTransfer();
+        private string _tmpDir;
+        private int _countdown = 10;
+        private bool _silent;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Icon = Resources.LMMSUpdater;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _silent = Environment.CommandLine.ContainsEx("/silent");
+            Text = Resources.WindowTitle;
+            TaskBar.Progress.SetState(Handle, TaskBar.Progress.Flags.Indeterminate);
+
+            string updUrl = null;
+            try
+            {
+                var regexUrl = Resources.RegexUrl;
+                if (!Ini.ReadDirect("Settings", "BetaUpdates").EqualsEx("1", "True"))
+                    regexUrl += "/latest";
+                var source = NetEx.Transfer.DownloadString(regexUrl);
+                if (string.IsNullOrWhiteSpace(source))
+                    throw new ArgumentNullException(nameof(source));
+                source = TextEx.FormatNewLine(source);
+#if x86
+                const string arch = "32";
+#else
+                const string arch = "64";
+#endif
+                source = source.SplitNewLine().Where(x => x.ContainsEx(Resources.SearchPrefix) && x.ContainsEx(string.Format(Resources.SearchSuffix, arch))).Take(1).Join();
+                foreach (Match match in Regex.Matches(source, Resources.RegexPattern, RegexOptions.Singleline))
+                {
+                    var mPath = match.Groups[1].ToString();
+                    if (string.IsNullOrWhiteSpace(mPath) || mPath.Count(c => c == '/') != 1)
+                        continue;
+                    updUrl = string.Format(Resources.UpdateUrl, mPath);
+                    break;
+                }
+                if (!NetEx.FileIsAvailable(updUrl, 60000, Resources.UserAgent))
+                    throw new PathNotFoundException(updUrl);
+            }
+            catch (Exception ex)
+            {
+                Log.Write(ex);
+                if (!_silent)
+                    MessageBoxEx.Show(Resources.Msg_Warn_01, Resources.WindowTitle, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Exit();
+            }
+
+            var appPath = PathEx.Combine(Resources.AppPath);
+            var localDate = File.GetLastWriteTime(appPath);
+            var onlineDate = NetEx.GetFileDate(updUrl, Resources.UserAgent);
+            if ((onlineDate - localDate).Days > 0)
+            {
+                if (_silent || MessageBoxEx.Show(Resources.Msg_Hint_00, Resources.WindowTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    var archivePath = PathEx.Combine(PathEx.LocalDir, $"..\\{PathEx.GetTempFileName()}");
+                    if (!File.Exists(archivePath))
+                    {
+                        _tmpDir = PathEx.Combine(Path.GetTempPath(), PathEx.GetTempDirName(Resources.AppName));
+                        var hlpPath = Path.Combine(_tmpDir, "7z.zip");
+                        ResourcesEx.Extract(Resources._7z, hlpPath, true);
+                        Compaction.Unzip(hlpPath, _tmpDir);
+                        _transfer.DownloadFile(updUrl, archivePath);
+                        Opacity = 1f;
+                        CheckDownload.Enabled = true;
+                        return;
+                    }
+                    ExtractDownload.RunWorkerAsync();
+                }
+                Application.Exit();
+                return;
+            }
+            if (!_silent)
+                MessageBoxEx.Show(Resources.Msg_Hint_01, Resources.WindowTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.Exit();
+        }
+
+        private void CheckDownload_Tick(object sender, EventArgs e)
+        {
+            DLSpeed.Text = _transfer.TransferSpeedAd;
+            DLPercentage.Value = _transfer.ProgressPercentage;
+            DLLoaded.Text = _transfer.DataReceived;
+            if (!_transfer.IsBusy)
+                _countdown--;
+            if (_countdown == 9)
+                DLPercentage.JumpToEnd();
+            TaskBar.Progress.SetValue(Handle, DLPercentage.Value, DLPercentage.Maximum);
+            if (_countdown > 0)
+                return;
+            CheckDownload.Enabled = false;
+            ExtractDownload.RunWorkerAsync();
+        }
+
+        private void ExtractDownload_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var updDir = PathEx.Combine(PathEx.LocalDir, $"..\\{PathEx.GetTempDirName()}");
+            try
+            {
+                if (_transfer?.FilePath == null || !File.Exists(_transfer.FilePath))
+                    return;
+                Compaction.Zip7Helper.ExePath = PathEx.Combine(_tmpDir, "7z.exe");
+                if (!File.Exists(Compaction.Zip7Helper.ExePath))
+                    throw new FileNotFoundException();
+                if (!Directory.Exists(updDir))
+                    Directory.CreateDirectory(updDir);
+                using (var p = Compaction.Zip7Helper.Unzip(_transfer.FilePath, updDir, ProcessWindowStyle.Minimized))
+                    if (!p?.HasExited == true)
+                        p?.WaitForExit();
+                var updFiles = Directory.GetFiles(updDir, "*", SearchOption.TopDirectoryOnly);
+                if (updFiles.Length == 0)
+                    throw new ArgumentNullException(nameof(updFiles));
+                var appPath = Directory.EnumerateFiles(updDir, $"*{Resources.AppName}.exe", SearchOption.AllDirectories).FirstOrDefault();
+                if (string.IsNullOrEmpty(appPath))
+                    throw new ArgumentNullException(nameof(appPath));
+                var curFiles = Directory.EnumerateFiles(PathEx.LocalDir, "*", SearchOption.AllDirectories).Where(x => !Resources.WhiteList.ContainsEx(Path.GetFileName(x)));
+                foreach (var file in curFiles)
+                    try
+                    {
+                        File.Delete(file);
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            File.Move(file, $"{file}.{{{Path.GetDirectoryName(updDir)}}}");
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Write(ex);
+                        }
+                    }
+                Data.DirCopy(updDir, PathEx.LocalDir, true, true);
+                if (Directory.Exists(updDir))
+                    Directory.Delete(updDir, true);
+                var redundantData = new[]
+                {
+                    PathEx.Combine(PathEx.LocalDir, "$PLUGINSDIR"),
+                    PathEx.Combine(PathEx.LocalDir, "Uninstall.exe")
+                };
+                foreach (var data in redundantData)
+                {
+                    if (!PathEx.DirOrFileExists(data))
+                        continue;
+                    try
+                    {
+                        if (Data.IsDir(data))
+                            Directory.Delete(data, true);
+                        else
+                            File.Delete(data);
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Write(ex);
+                    }
+                }
+                appPath = PathEx.Combine(Resources.AppPath);
+                if (File.Exists(appPath))
+                    File.SetLastWriteTime(appPath, DateTime.Now);
+                e.Result = true;
+            }
+            catch (Exception ex)
+            {
+                Log.Write(ex);
+                e.Result = false;
+                Data.ForceDelete(updDir);
+            }
+        }
+
+        private void ExtractDownload_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+            TaskBar.Progress.SetState(Handle, TaskBar.Progress.Flags.Indeterminate);
+            if (!_silent)
+                switch (e.Result as bool?)
+                {
+                    case true:
+                        MessageBoxEx.Show(Resources.Msg_Hint_02, Resources.WindowTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+                    default:
+                        MessageBoxEx.Show(Resources.Msg_Warn_01, Resources.WindowTitle, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        break;
+                }
+            Application.Exit();
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            var appPath = PathEx.Combine(Resources.AppPath);
+            Ini.WriteDirect("History", "LastCheck", File.Exists(appPath) ? DateTime.Now : DateTime.MinValue);
+            ProcessEx.SendHelper.WaitThenDelete(_tmpDir, 5, Elevation.IsAdministrator);
+            ProcessEx.SendHelper.WaitThenDelete(_transfer.FilePath, 5, Elevation.IsAdministrator);
+        }
+    }
+}
---
+++ Helper for third party apps/LMMSUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if x86
+[assembly: AssemblyTitle("LMMS Media Player Updater")]
+[assembly: AssemblyProduct("LMMSUpdater")]
+#else
+[assembly: AssemblyTitle("LMMS Media Player Updater (64-bit)")]
+[assembly: AssemblyProduct("LMMSUpdater64")]
+#endif
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Si13n7 Dev. ®")]
+[assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2017")]
+[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d11b001a-08c9-421b-bcaa-b7c4809ce734")]
+
+[assembly: AssemblyVersion("17.6.13.*")]
